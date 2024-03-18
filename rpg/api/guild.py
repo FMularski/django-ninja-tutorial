77ad45aa7ecce95cbf051a04a5f36fa85fd72917
Filip Mularski <mularskif@gmail.com>
2024-03-18 10:36:29 +0100

    implemented the rest of crud guild api

--- rpg/api/guild.py
+++ rpg/api/guild.py
@@ -2,11 +2,13 @@ from http import HTTPStatus
 
 from django.db.models import F, Value
 from django.db.models.functions import Length, StrIndex, Substr
-from ninja import Query, Router
+from django.shortcuts import get_object_or_404
+from ninja import File, Query, Router
+from ninja.files import UploadedFile
 from ninja.pagination import PageNumberPagination, paginate
 
 from rpg.models import Guild
-from rpg.schemas import GuildFilterSchema, GuildOrderSchema, GuildReadSchema
+from rpg.schemas import GuildFilterSchema, GuildOrderSchema, GuildReadSchema, GuildWriteSchema
 
 router = Router(tags=["Guilds"])
 
@@ -26,4 +28,38 @@ def get_guilds(
     filtered = filters.filter(guilds)
     ordered = ordering.order(filtered)
 
-    return ordered
+    return HTTPStatus.OK, ordered
+
+
+@router.get("{pk}/", response={HTTPStatus.OK: GuildReadSchema})
+def get_guild(request, pk: int):
+    return HTTPStatus.OK, get_object_or_404(Guild, pk=pk)
+
+
+@router.post("", response={HTTPStatus.CREATED: GuildReadSchema})
+def post_guild(request, guild: GuildWriteSchema, banner: UploadedFile = File()):
+    guild_data = guild.model_dump()
+    return HTTPStatus.CREATED, Guild.objects.create(**guild_data, banner=banner)
+
+
+@router.api_operation(["PUT", "PATCH"], "{pk}/", response={HTTPStatus.OK: GuildReadSchema})
+def update_guild(request, pk: int, guild: GuildWriteSchema, banner: UploadedFile = File(None)):
+    existing_guild = get_object_or_404(Guild, pk=pk)
+    guild_data = guild.model_dump()
+
+    for k, v in guild_data.items():
+        setattr(existing_guild, k, v)
+
+    if banner:
+        existing_guild.banner = banner
+
+    existing_guild.save()
+    return HTTPStatus.OK, existing_guild
+
+
+@router.delete("{pk}/", response={HTTPStatus.NO_CONTENT: None})
+def delete_guild(request, pk: int):
+    existing_guild = get_object_or_404(Guild, pk=pk)
+    existing_guild.delete()
+
+    return HTTPStatus.NO_CONTENT, None
--- rpg/schemas/guild.py
+++ rpg/schemas/guild.py
@@ -1,5 +1,6 @@
 from django.db.models import Q
 from ninja import Field, FilterSchema, ModelSchema, Schema
+from pydantic import validator
 
 from core.schemas import OrderSchema
 from rpg import models
@@ -24,7 +25,7 @@ class GuildReadSchema(ModelSchema):
         return BannerDetailSchema.model_validate(
             {
                 "filename": obj.banner.name.split("/")[-1],
-                "ext": obj.banner_ext,
+                "ext": obj.banner.name.split(".")[-1],
                 "size": obj.banner.size,
                 "url": obj.banner.url,
             }
@@ -41,3 +42,14 @@ class GuildFilterSchema(FilterSchema):
 
     def filter_banner_ext(cls, banner_ext: str) -> Q:
         return Q(banner_ext__iexact=banner_ext) if banner_ext else Q()
+
+
+class GuildWriteSchema(Schema):
+    name: str
+
+    @validator("name")
+    def validate_name(cls, name: str):
+        if "banned" in name.lower():
+            raise ValueError("Name contains a banned word.")
+
+        return name
