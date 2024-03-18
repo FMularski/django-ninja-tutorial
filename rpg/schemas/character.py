268d21ed9e7970c4342ed69917b78f6d91138ac6
Filip Mularski <mularskif@gmail.com>
2024-03-18 14:50:15 +0100

    implemented ExperienceSchema and nested it in CharacterReadSchema

--- rpg/schemas/character.py
+++ rpg/schemas/character.py
@@ -1,23 +1,48 @@
 from django.db.models import Q
-from ninja import Field, FilterSchema, ModelSchema
+from ninja import Field, FilterSchema, ModelSchema, Schema
 
 from core.schemas import OrderSchema
 from rpg import models
 
 from .characterclass import CharacterClassReadSchema
+from .guild import GuildReadSchema
 from .quest import QuestReadSchema
+
+
+class ExperienceSchema(Schema):
+    experience: int
+    max_experience: int
+    percent_experience: float = None
+
+    @staticmethod
+    def resolve_percent_experience(obj):
+        return obj.experience / obj.max_experience
 
 
 class CharacterReadSchema(ModelSchema):
     character_class: CharacterClassReadSchema
     quests: list[QuestReadSchema]
+    guild: GuildReadSchema
+    experience: ExperienceSchema
     # TODO:
-    # guild = schemas.GuildReadSchema
     # statistics = schemas.StatisticsReadSchema
 
     class Meta:
         model = models.Character
-        fields = "__all__"
+        fields = (
+            "name",
+            "character_class",
+            "statistics",
+            "guild",
+            "gold",
+            "quests",
+            "level",
+            "experience",
+        )
+
+    @staticmethod
+    def resolve_experience(obj):
+        return ExperienceSchema.model_validate(obj)
 
 
 class CharacterOrderSchema(OrderSchema):
